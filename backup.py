2353ab4804736d6f31eb2b09df2d13368599fa2f
tcrouzet <tc@tcrouzet.com>
2023-12-24 17:47:51 +0100

    Working

--- backup.py
+++ backup.py
@@ -4,6 +4,8 @@ import datetime
 import pprint
 import plistlib
 import xml.etree.ElementTree as ET
+import json
+
 
 #Where to backup on the Mac - desktop by default
 backup_dir = '~/Desktop/Ubackup/'
@@ -129,28 +131,36 @@ def convert_ulysses_to_markdown(xml_content):
 
     return markdown.strip()
 
-def build_md_path(filename):
+def build_path(filename,ext=".md"):
     global saved_path    
-    md_file= os.path.join(markdown_dir,saved_path,filename+".md")
+    md_file= os.path.join(markdown_dir,saved_path,filename+ext)
     destination_dir = os.path.dirname(md_file)
     os.makedirs(destination_dir, exist_ok=True)
     return md_file
 
+def update_saved_path(id,flag):
+    global saved_path
+
+    if flag:
+        # Remplacer le dernier segment du chemin par l'identifiant
+        path_parts = saved_path.split(os.sep)[:-1]  # Enlever le dernier segment
+        path_parts.append(id)
+        saved_path = os.sep.join(path_parts)
+    else:
+        saved_path = os.path.join(saved_path, id)
 
 def analyser_ulgroup(file):
     # Vérifier si le résultat est déjà dans le cache
     if file in cache_noms_dossiers:
         return cache_noms_dossiers[file]
 
-    try:
-        with open(file, 'rb') as f:
-            contenu = plistlib.load(f)
-            nom_dossier = contenu.get('displayName', 'Nom Inconnu')
-            child_order = contenu.get('childOrder', [])
-            cache_noms_dossiers[file] = (nom_dossier, child_order)
-            return (nom_dossier, child_order)
-    except Exception as e:
-        print(f"Error reading {file}: {e}")
+    contenu = plist_loader(file)
+    if contenu:
+        nom_dossier = contenu.get('displayName', 'Nom Inconnu')
+        child_order = contenu.get('childOrder', [])
+        cache_noms_dossiers[file] = (nom_dossier, child_order)
+        return (nom_dossier, child_order)
+    else:
         return ('Unknown Name', [])
 
 def real_dir_names(file):
@@ -174,49 +184,55 @@ def real_dir_names(file):
 
     return "/".join(noms_dossiers)
 
-import os
-
-def find_child_number(chemin_dossier_ulysses):
-    # Extraire le dossier parent
-    dossier_parent = os.path.dirname(chemin_dossier_ulysses)
-
-    # Construire le chemin vers le fichier Info.ulgroup
-    fichier_ulgroup = os.path.join(dossier_parent, 'Info.ulgroup')
-
-    # Analyser le fichier ulgroup pour obtenir le nom et l'ordre des enfants
-    nom_dossier, child_order = analyser_ulgroup(fichier_ulgroup)
-
-    # Identifier le nom spécifique du dossier .ulysses dans le chemin
-    nom_ulysses = os.path.basename(chemin_dossier_ulysses)
-
+def plist_loader(path):
     try:
-        # Trouver la position du dossier .ulysses dans child_order
-        child_number = str(child_order.index(nom_ulysses) + 1).zfill(3)
-    except ValueError:
-        # Si le dossier .ulysses n'est pas trouvé dans child_order
-        child_number = "000"
+        with open(path, 'rb') as f:
+            contenu = plistlib.load(f)
+            return contenu
 
-    return child_number
+    except Exception as e:
+        print(f"Error reading file {path}: {e}")
+        return False
+
+def metadata_id(path):
+    path = path.replace("/Metadata.plist", "")
+    return os.path.basename(path).split('.')[0]
+
+def find_oder(id):
+    global ulgroup_data
+    #print(ulgroup_data)
+    if "sheetClusters" in ulgroup_data:
+        total_length = len(ulgroup_data["sheetClusters"])
+        max_digits = len(str(total_length))
+
+        id += ".ulysses"
+
+        for i, cluster in enumerate(ulgroup_data["sheetClusters"]):
+            if id in cluster:
+                return str(i + 1).zfill(max_digits)
+    return "0"
 
 def sort_files(filename):
     if filename.endswith('.ulgroup'):
         return (0, filename)
     elif filename.endswith('.ulysses'):
         return (1, filename)
-    else:
+    elif filename.endswith('.plist'):
         return (2, filename)
+    else:
+        return (3, filename)
     
 os.system('clear')
 
 #secure_backup()
 
-data_file_extensions = {'.png', '.jpg', '.jpeg', '.tiff'}
+data_file_extensions = {'.png', '.jpg', '.jpeg', '.tiff', '.pdf'}
 total_txt = 0
 total_xml = 0
 total_empty = 0
 saved_path = ""
-content_index = 0
-child_number = ''
+plist_flag = False
+ulgroup_data = {}
 
 for dirpath, dirnames, filenames in os.walk(Ulysses_dir):
 
@@ -228,7 +244,10 @@ for dirpath, dirnames, filenames in os.walk(Ulysses_dir):
         # Construire le chemin complet du fichier
         filepath = os.path.join(dirpath, filename)
 
+        #print(filename)
+
         if filename.startswith('.'):
+            #print(filename)
             continue
 
         elif filename.endswith('.txt'):
@@ -239,19 +258,13 @@ for dirpath, dirnames, filenames in os.walk(Ulysses_dir):
             #XML Ulysses Markdown
             total_xml += 1
 
-            local_dir = filepath.replace("/Content.xml","")
-            #print(local_dir)
-            child_number = find_child_number(local_dir)
-
             with open(filepath) as f:
                 xml = f.read()
             
             markdown = convert_ulysses_to_markdown(xml)
             if len(markdown)>0:
 
-                content_index +=1
-                md_file = build_md_path(f"{child_number}-content{content_index}")
-                #print(md_file)
+                md_file = build_path(f"{order}-content")
                 
                 with open(md_file, 'w', encoding='utf-8') as f:
                     f.write(markdown)
@@ -261,20 +274,38 @@ for dirpath, dirnames, filenames in os.walk(Ulysses_dir):
                 total_empty += 1
 
         elif filename.endswith('.ulgroup'):
-            #Plist file
+            #Plist file describing folders and sub-folders
+            #exit("ulgroup")
             saved_path = real_dir_names(filepath)
-            content_index = 0
+            order = "0"
+            plist_flag = False
+            ulgroup_data = plist_loader(filepath)
+            #Plist backup
+            ulgroup_str = json.dumps(ulgroup_data)
+            ulgroup_file = build_path("Info",".txt")
+            with open(ulgroup_file, 'w', encoding='utf-8') as f:
+                f.write(ulgroup_str)
 
         elif filename.endswith('.plist'):
-            #File versioning
-            pass
+            #Metada of text/media contener
+            #print(filepath)
+            id = metadata_id(filepath)
+            order = find_oder(id)
+            plist_flag = True
+            plist_data = plist_loader(filepath)
+            plist_str = json.dumps(plist_data)
+            plist_file = build_path(f"plist/{order}",".txt")
+            with open(plist_file, 'w', encoding='utf-8') as f:
+                f.write(plist_str)
 
         elif os.path.splitext(filename)[1].lower() in data_file_extensions:
             #Images, PDF and other files attached to projects
-            pass
+            media_file = build_path(f"media-{order}/",filename)
+            shutil.copy2(filepath, media_file)
    
         else:
             #Trash
+            #print("Trash",filename)
             continue
 
 print("txt:",total_txt)
